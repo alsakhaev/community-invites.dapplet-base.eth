6b4defc0596ca4d4f92f2d7933f961400a46fedd
Alexander Sakhaev <alsakhaev@gmail.com>
2020-09-09 14:24:07 +0300

    feat: added dapplet skeleton with label

---
+++ dapplet/src/index.ts
@@ -0,0 +1,34 @@
+import { } from '@dapplets/dapplet-extension';
+import ICON from './icons/icon.png';
+
+@Injectable
+export default class Feature {
+    private _overlay;
+
+    constructor(
+        @Inject("identity-adapter.dapplet-base.eth")
+        public identityAdapter: any, // ITwitterAdapter;
+
+        @Inject("common-adapter.dapplet-base.eth")
+        public viewportAdapter: any
+    ) {
+        const wallet = Core.wallet();
+        Core.storage.get('overlayUrl').then(url => this._overlay = Core.overlay({ url, title: 'Identity Management' }));
+
+        const { statusLine } = viewportAdapter.exports;
+        const { label, button } = this.identityAdapter.exports;
+
+        this.identityAdapter.attachConfig({
+            POST_USERNAME_LABEL: [
+                label({
+                    initial: "DEFAULT",
+                    "DEFAULT": {
+                        text: 'Devcon 2020 (+1)',
+                        exec: () => console.log('Label clicked'),
+                        img: ICON
+                    }
+                })
+            ]
+        })
+    }
+}
