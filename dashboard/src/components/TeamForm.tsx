548fe8c61db70fff5305bb00c8ccab051c3ac1c0
Alexander Sakhaev <alsakhaev@gmail.com>
2021-01-25 18:48:56 +0300

    feat(dashboard): added copy button

--- dashboard/src/components/TeamForm.tsx
+++ dashboard/src/components/TeamForm.tsx
@@ -14,6 +14,7 @@ interface IState {
     tags: string;
     id: string | null;
     isLoading: boolean;
+    copied: boolean;
 }
 
 export class TeamForm extends React.Component<IProps, IState> {
@@ -26,7 +27,8 @@ export class TeamForm extends React.Component<IProps, IState> {
             name: '',
             tags: '',
             id: null,
-            isLoading: false
+            isLoading: false,
+            copied: false
         };
     }
 
@@ -61,7 +63,20 @@ export class TeamForm extends React.Component<IProps, IState> {
                     {(s.id) ? <Message
                         success
                         header='Team Created'
-                        content={`Private ID of your team: ${s.id}`}
+                        content={<span>
+                            Private ID of your team: {s.id}
+                            <Icon
+                                style={{ marginLeft: '4px' }}
+                                name={(this.state.copied) ? 'checkmark' : 'copy outline'}
+                                link
+                                title={(this.state.copied) ? 'Copied' : 'Copy'}
+                                onClick={() => {
+                                    s.id && navigator.clipboard.writeText(s.id);
+                                    this.setState({ copied: true });
+                                    setTimeout(() => this.setState({ copied: false }), 3000);
+                                }}
+                            />
+                        </span>}
                     /> :
                         <Form loading={s.isLoading}>
                             <Form.Input
